b3fd5e3d192d246d0e30f86eb2cf798f07053b28
Alessandro <alessandro.portosa@eng.it>
2017-11-10 16:37:25 +0100

    Add version 6.1.1

---
+++ 6.1.1/README.md
@@ -0,0 +1,2 @@
+# Knowage-Server-Chef
+Chef deployment script for Knowage Server https://www.knowage-suite.com
---
+++ 6.1.1/knowage/metadata.rb
@@ -0,0 +1,13 @@
+name 'knowage'
+maintainer 'Knowage'
+maintainer_email 'marco.cortella@eng.it'
+license 'All Rights Reserved'
+description 'Installs/Configures knowage'
+long_description 'Installs/Configures knowage'
+version '0.1.0'
+chef_version '>= 12.1' if respond_to?(:chef_version)
+
+depends 'docker', '~> 2.0'
+depends 'docker_compose', '~> 0.0'
+
+
