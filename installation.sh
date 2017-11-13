20a6378a1ebf6cd89b956049787b6f506078ccbf
Alessandro <alessandro.portosa@eng.it>
2017-11-13 11:05:02 +0100

    Update installation.sh

--- installation.sh
+++ installation.sh
@@ -7,7 +7,7 @@ dpkg -i chef_13.5.3-1_amd64.deb
 rm -f chef_13.5.3-1_amd64.deb
 
 #download recipes
-wget https://github.com/KnowageLabs/Knowage-Server/releases/download/6.1/chef-cookbooks.zip
+wget https://github.com/KnowageLabs/Knowage-Server-Chef/releases/download/6.1.1/chef-cookbooks.zip
 
 #install zip
 apt-get install unzip
@@ -16,5 +16,5 @@ unzip chef-cookbooks.zip
 
 #install knowage
 mkdir /etc/chef
-chef-client -z -o 'recipe[knowage::1.0.4_install]'
+chef-client -z -o 'recipe[knowage::1.0.0_install]'
 rm -rf cookbooks
