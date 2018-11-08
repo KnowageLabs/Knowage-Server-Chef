3deb955d6b04a795804070fa9e691adf76e4dfe2
DraganPirkovic <41570057+DraganPirkovic@users.noreply.github.com>
2018-11-08 15:12:43 +0100

    Update installation.sh

--- bin/installation.sh
+++ bin/installation.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 #Knowage VM installation script
+usage() { echo "Usage: $0 [-a <ip address>] [-v <version>]"; exit 1; }
+while getopts ":a:v:" OPTION
+do
+     case $OPTION in
+	a)
+	address=$OPTARG
+	;;
+        v)
+	version=$OPTARG 
+        ;; 
+        ?)
+        usage
+        ;;
+     esac
+	  
+done
 
 #install chef 13 client
 wget https://packages.chef.io/files/stable/chef/13.5.3/ubuntu/16.04/chef_13.5.3-1_amd64.deb
@@ -18,13 +34,20 @@ fi
 cp cookbooks/knowage/files/docker-compose.yml.bak cookbooks/knowage/files/docker-compose.yml
 
 #check if the script has been started without a specific IP address
-if [[ -z "$1" ]]; then
+if [[ -z $address ]]; then
         #remove PUBLIC_ADDRESS env attribute
         sed -i "/- PUBLIC_ADDRESS=.*/d" cookbooks/knowage/files/docker-compose.yml
 fi
 
+# choosing a knowage version
+if [[ -z $version ]]; then
+sed -i "s|knowagelabs/knowage-server-docker|knowagelabs/knowage-server-docker:$version|" cookbooks/knowage/files/docker-compose.yml
+fi
+
 #replace the local IP (127.0.0.1) with the provided one, if any
-sed -i "s|- PUBLIC_ADDRESS=.*|- PUBLIC_ADDRESS=${1}|" cookbooks/knowage/files/docker-compose.yml
+sed -i "s|- PUBLIC_ADDRESS=.*|- PUBLIC_ADDRESS=$address|" cookbooks/knowage/files/docker-compose.yml
+
+
 
 #install knowage
 if [ ! -d "/etc/chef" ]; then
