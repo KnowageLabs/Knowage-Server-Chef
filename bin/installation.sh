d82a0d719eb6437b1d45b98d2eb566b9a62fed70
Alessandro <alessandro.portosa@eng.it>
2018-11-09 15:45:52 +0100

    Update installation.sh

--- bin/installation.sh
+++ bin/installation.sh
@@ -25,43 +25,32 @@ wget https://packages.chef.io/files/stable/chef/13.5.3/ubuntu/16.04/chef_13.5.3-
 dpkg -i chef_13.5.3-1_amd64.deb
 rm -f chef_13.5.3-1_amd64.deb
 
-
-
-
 if [[ ! -d "cookbooks/knowage" ]]; then
-apt-get install git-core
-git clone https://github.com/KnowageLabs/Knowage-Server-Chef.git
-cd Knowage-Server-Chef
-git checkout parametric-recipe
-cd ..
-mv Knowage-Server-Chef/cookbooks .
-rm -rf Knowage-Server-Chef
-
-        #download recipes
-        #wget https://github.com/KnowageLabs/Knowage-Server-Chef/releases/download/6.1.1/chef-cookbooks.zip
-        #install zip
-        #apt-get install unzip
-        #unzip chef-cookbooks.zip
+	apt-get install git-core
+	git clone https://github.com/KnowageLabs/Knowage-Server-Chef.git
+	cd Knowage-Server-Chef
+	git checkout parametric-recipe
+	cd ..
+	mv Knowage-Server-Chef/cookbooks .
+	rm -rf Knowage-Server-Chef
 fi
 
 #restore original docker-compose file configuration
 cp cookbooks/knowage/files/docker-compose.yml.bak cookbooks/knowage/files/docker-compose.yml
 
 #check if the script has been started without a specific IP address
-if [[ -z $address ]]; then
+if [[ -z "$address" ]]; then
         #remove PUBLIC_ADDRESS env attribute
         sed -i "/- PUBLIC_ADDRESS=.*/d" cookbooks/knowage/files/docker-compose.yml
 fi
 
 # choosing a knowage version
-if [[ -z $version ]]; then
-sed -i "s|knowagelabs/knowage-server-docker|knowagelabs/knowage-server-docker:$version|" cookbooks/knowage/files/docker-compose.yml
+if [[ -n "$version" ]]; then
+	sed -i "s|knowagelabs/knowage-server-docker|knowagelabs/knowage-server-docker:$version|" cookbooks/knowage/files/docker-compose.yml
 fi
 
 #replace the local IP (127.0.0.1) with the provided one, if any
 sed -i "s|- PUBLIC_ADDRESS=.*|- PUBLIC_ADDRESS=$address|" cookbooks/knowage/files/docker-compose.yml
-
-
 
 #install knowage
 if [ ! -d "/etc/chef" ]; then
